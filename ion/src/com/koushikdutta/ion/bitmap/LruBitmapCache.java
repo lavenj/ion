9fb0320b7ccd8dc1ef5157a86bfc20ef02f533f0
jake <lavenj@gmail.com>
2013-11-22 01:52:42 -0500

    recycle bitmaps on removal

--- ion/src/com/koushikdutta/ion/bitmap/LruBitmapCache.java
+++ ion/src/com/koushikdutta/ion/bitmap/LruBitmapCache.java
@@ -1,11 +1,26 @@
 package com.koushikdutta.ion.bitmap;
 
+import android.graphics.Bitmap;
+import android.util.Log;
+
 class LruBitmapCache extends LruCache<String, BitmapInfo> {
     public LruBitmapCache(int maxSize) {
         super(maxSize);
     }
 
-    @Override
+	@Override
+	protected void entryRemoved(boolean evicted, String key, BitmapInfo oldValue, BitmapInfo newValue) {
+		super.entryRemoved(evicted, key, oldValue, newValue);
+
+		if( oldValue.bitmaps != null ) {
+			for(Bitmap b:oldValue.bitmaps) {
+				Log.i("LruCache", "Recycling entry in " + key);
+				b.recycle();
+			}
+		}
+	}
+
+	@Override
     protected int sizeOf(String key, BitmapInfo info) {
         return info.sizeOf();
     }
